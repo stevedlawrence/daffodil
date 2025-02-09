f0cb03780f76f3f957200b64f074fa2375901e9e
Steve Lawrence <slawrence@apache.org>
2025-02-09 15:56:31 -0500

    DAFFODIL-XXX

--- .github/actions/asf-release-candidate/post.js
+++ .github/actions/asf-release-candidate/post.js
@@ -26,60 +26,60 @@ const { exec } = require('@actions/exec');
 // The post-if condition in action.yml ensures this is only ever run if a job
 // succeeds.
 async function run() {
-	const project_name = core.getInput("project_name", { required: true });
-	const svn_username = core.getInput("svn_username", { required: true });
-	const svn_password = core.getInput("svn_password", { required: true });
+	try {
+		const project_name = core.getInput("project_name", { required: true });
+		const svn_username = core.getInput("svn_username", { required: true });
+		const svn_password = core.getInput("svn_password", { required: true });
 
-	const artifact_dir = core.getState("artifact_dir");
-	const gpg_signing_key_id = core.getState("gpg_signing_key_id");
-	const publish = core.getState("publish") === "true";
-	const release_version = core.getState("release_version");
+		const artifact_dir = core.getState("artifact_dir");
+		const gpg_signing_key_id = core.getState("gpg_signing_key_id");
+		const publish = core.getState("publish") === "true";
+		const release_version = core.getState("release_version");
 
-	// sign/checksum all artifacts
-	const artifacts = fs.readdirSync(artifact_dir, { recursive: true, withFileTypes: true });
-	for(const artifact of artifacts) {
-		if (artifact.isFile()) {
-			// must sign rpms before sha/gpg since rpmsign modifies the RPM
-			if (artifact.name.endsWith(".rpm")) {
-				await exec("rpmsign", ["--define", `_gpg_name ${ gpg_signing_key_id }`, "--define", "_binary_filedigest_algorithm 10", "--addsign", `${ artifact.parentPath }/${ artifact.name }`]);
-			}
-			let checksum = "";
-			await exec("sha512sum", ["--binary", artifact.name], {
-				cwd: artifact.parentPath,
-				listeners: {
-					stdout: (data) => { checksum += data.toString(); }
+		// sign/checksum all artifacts
+		const artifacts = fs.readdirSync(artifact_dir, { recursive: true, withFileTypes: true });
+		for(const artifact of artifacts) {
+			if (artifact.isFile()) {
+				// must sign rpms before sha/gpg since rpmsign modifies the RPM
+				if (artifact.name.endsWith(".rpm")) {
+					await exec("rpmsign", ["--define", `_gpg_name ${ gpg_signing_key_id }`, "--define", "_binary_filedigest_algorithm 10", "--addsign", `${ artifact.parentPath }/${ artifact.name }`]);
 				}
-			});
-			fs.appendFileSync(`${ artifact.name }.sha512`, checksum);
-			await exec("gpg", ["--default-key", gpg_signing_key_id, "--batch", "--yes", "--detach-sign", "--armor", "--output", `${ artifact.name }.asc`, artifact.name], {
-				cwd: artifact.parentPath
+				let checksum = "";
+				await exec("sha512sum", ["--binary", artifact.name], {
+					cwd: artifact.parentPath,
+					listeners: {
+						stdout: (data) => { checksum += data.toString(); }
+					}
+				});
+				fs.appendFileSync(`${ artifact.name }.sha512`, checksum);
+				await exec("gpg", ["--default-key", gpg_signing_key_id, "--batch", "--yes", "--detach-sign", "--armor", "--output", `${ artifact.name }.asc`, artifact.name], {
+					cwd: artifact.parentPath
+				});
+			}
+		}
+
+		if (publish) {
+			await exec("svn", ["add", artifact_dir]);
+			await exec("svn", ["commit", "--username", svn_username, "--password", svn_password, "--message", `Stage ${ project_name } ${ release_version }`, artifact_dir]);
+		} else {
+			// if publishing was disabled then this action was likely just triggered
+			// just for testing, so upload the maven-local and artifact directories so
+			// they can be verified
+			const release_candidate_dir = `${ os.tmpdir() }/release-candidate`;
+			const upload_artifacts = fs.readdirSync(release_candidate_dir, { recursive: true, withFileTypes: true })
+				.filter((dirent) => dirent.isFile())
+				.filter((dirent) => !dirent.parentPath.split("/").includes(".svn"))
+				.map((dirent) => `${ dirent.parentPath }/${ dirent.name }`);
+			const artifact_client = new DefaultArtifactClient();
+			artifact_client.uploadArtifact(`release-candidate`, upload_artifacts, os.tmpdir(), {
+				compressionLevel: 0,
+				retentionDays: 1
 			});
 		}
-	}
 
-	if (publish) {
-		await exec("svn", ["add", artifact_dir]);
-		await exec("svn", ["commit", "--username", svn_username, "--password", svn_password, "--message", `Stage ${ project_name } ${ release_version }`, artifact_dir]);
-	} else {
-		// if publishing was disabled then this action was likely just triggered
-		// just for testing, so upload the maven-local and artifact directories so
-		// they can be verified
-		const release_candidate_dir = `${ os.tmpdir() }/release-candidate`;
-		const upload_artifacts = fs.readdirSync(release_candidate_dir, { recursive: true, withFileTypes: true })
-			.filter((dirent) => dirent.isFile())
-			.filter((dirent) => !dirent.parentPath.split("/").includes(".svn"))
-			.map((dirent) => `${ dirent.parentPath }/${ dirent.name }`);
-		const artifact_client = new DefaultArtifactClient();
-		artifact_client.uploadArtifact(`release-candidate`, upload_artifacts, os.tmpdir(), {
-			compressionLevel: 0,
-			retentionDays: 1
-		});
+	} catch (error) {
+		core.setFailed(error.message);
 	}
 }
 
-
-try {
-	run();
-} catch (error) {
-	core.setFailed(error.message);
-}
+run();
